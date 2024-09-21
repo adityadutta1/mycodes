815b6f1e2b7ea0040448f5094ef3a214f627b89e
aditya dutta <duttaaditya698@gmail.com>
2024-09-21 09:47:33 +0530

    binarySearch

--- com/BinarySearch/RotatedSortedArray.java
+++ com/BinarySearch/RotatedSortedArray.java
@@ -8,27 +8,38 @@ public class RotatedSortedArray {
             while(left<=right)
             {
                 int mid=(left+right)/2;
+                // Target value is present at the middle of the array
+
+
                 if(nums.get(mid)==target)
                     return mid;
+                // start to mid is sorted
                 if(nums.get(left)<=nums.get(mid))
                 {
                     if(target>=nums.get(left) && nums.get(mid)>target)
                     {
+                        // target is within the sorted first half of the array
+
                         right=mid-1;
+
                     }
                     else
                     {
+                        // target is not within the sorted first half, so let’s examine the unsorted second half
                         left=mid+1;
                     }
                 }
+                // mid to end is sorted
                 else
                 {
                     if(target<=nums.get(right) && nums.get(mid)<target)
                     {
+                        // target is within the sorted second half of the array
                         left=mid+1;
                     }
                     else
                     {
+                        // target is not within the sorted second half, so let’s examine the unsorted first half
                         right=mid-1;
                     }
                 }
